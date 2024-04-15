582e6702f42eb18ad7384c3a0e2906febe7ff0b7
Abhay Mishra <abhaym.cs.21@nitj.ac.in>
2024-04-15 19:52:47 +0530

    login and register

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -89,7 +89,7 @@ export default function LoginPage() {
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-xl">
         <form class="space-y-6" onSubmit={login} method="POST">
           <div>
-            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
             <div class="mt-2">
               <input id="username" value={username} onChange={ev => setUsername(ev.target.value)}  name="username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -76,7 +76,7 @@ export default function RegisterPage() {
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" onSubmit={register} method="POST">
           <div>
-            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
             <div class="mt-2">
               <input id="email" name="email"  value={username}  onChange={ev => setUsername(ev.target.value)} type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
